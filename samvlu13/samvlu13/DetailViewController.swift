5994249a75515d836d0d16130e6d14b23f7f9195
Khuevm <vngminhkhue@gmail.com>
2023-07-21 14:45:27 +0700

    Ex16,17,18

--- samvlu13/samvlu13/DetailViewController.swift
+++ samvlu13/samvlu13/DetailViewController.swift
@@ -18,9 +18,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let backButton = UIBarButtonItem(image: UIImage(systemName: "arrow.backward"), style: .plain, target: self, action: #selector(handleBackButton))
-        navigationItem.leftBarButtonItem = backButton
-        
+        addNavButton()
         
         guard let post = post else { return }
         navigationItem.title = post.title
@@ -30,7 +28,33 @@ class DetailViewController: UIViewController {
         hashtagLabel.text = post.hashtag
     }
     
+    private func addNavButton(){
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "arrow.backward"), style: .plain, target: self, action: #selector(handleBackButton))
+        navigationItem.leftBarButtonItem = backButton
+        
+        let moreButton = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, target: self, action: #selector(handleMoreButton))
+        navigationItem.rightBarButtonItem = moreButton
+    }
+    
     @objc private func handleBackButton(){
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func handleMoreButton(){
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "Report", style: .destructive)
+        let action2 = UIAlertAction(title: "Share", style: .default)
+        let action3 = UIAlertAction(title: "Save Image", style: .default)
+        let action4 = UIAlertAction(title: "Copy Link", style: .default)
+        let action5 = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addAction(action3)
+        alert.addAction(action4)
+        alert.addAction(action5)
+        
+        self.present(alert, animated: true)
     }
 }
